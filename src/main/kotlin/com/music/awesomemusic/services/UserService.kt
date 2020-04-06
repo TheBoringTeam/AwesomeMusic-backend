740e3e22a4c9eff917741a0c7e5a54392dee1fdc
HolkerDev <nick.penkovskiy@gmail.com>
2020-04-06 21:41:58 +0200

    Added roles to creating user feature

--- src/main/kotlin/com/music/awesomemusic/services/UserService.kt
+++ src/main/kotlin/com/music/awesomemusic/services/UserService.kt
@@ -1,17 +1,26 @@
 package com.music.awesomemusic.services
 
+import com.music.awesomemusic.domain.Role
 import com.music.awesomemusic.domain.dto.UserRegistrationForm
 import com.music.awesomemusic.models.AwesomeUser
+import com.music.awesomemusic.repositories.IRoleRepository
 import com.music.awesomemusic.repositories.IUserRepository
 import com.music.awesomemusic.utils.errors.UsernameExists
+import org.apache.log4j.Logger
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
 
 @Service
 class UserService {
+
+    private val logger = Logger.getLogger(UserService::class.java)
+
     @Autowired
     lateinit var userRepository: IUserRepository
+
+    @Autowired
+    lateinit var roleRepository: IRoleRepository
 
     @Autowired
     lateinit var passwordEncoder: PasswordEncoder
@@ -29,11 +38,31 @@ class UserService {
     }
 
     fun createUser(userFormDto: UserRegistrationForm) {
-        if (userRepository.existsByUsername(userFormDto.username)) {
-            throw UsernameExists("Provided username is already taken")
-        }
+        try {
+            logger.debug("Creating user was started")
+            if (userRepository.existsByUsername(userFormDto.username)) {
+                logger.debug("User with username ${userFormDto.username} already exists")
+                throw UsernameExists("Provided username is already taken")
+            }
 
-        val user = AwesomeUser(userFormDto.username, passwordEncoder.encode(userFormDto.password))
-        userRepository.save(user)
+            logger.debug("Find a USER role for the current user")
+            val role = roleRepository.findByRoleName("USER")
+
+            val user = AwesomeUser(userFormDto.username, passwordEncoder.encode(userFormDto.password))
+
+            // TODO : May be some optimization here (IDK rly)
+            val roles = user.roles as ArrayList<Role>
+            if (role.isPresent) {
+                roles.add(role.get())
+                logger.debug("Role for user ${userFormDto.username} was attached")
+            }
+
+            user.roles = roles
+            userRepository.save(user)
+            logger.debug("User ${user.username} was successfully created")
+        } catch (e: Exception) {
+            logger.error("Error while creating a new user: ${e.message}")
+            throw e
+        }
     }
 }
