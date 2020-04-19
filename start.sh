2529016de7ce2319fa2435ec81c5b8e031d9a3b8
HolkerDev <nick.penkovskiy@gmail.com>
2020-04-19 17:35:02 +0200

    Added logs to file

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 docker-compose up -d
-docker logs backend >& logs/backend.log
-docker logs postgres >& logs/postgres.log
+docker logs backend >> logs/backend.txt
+docker logs postgres >> logs/postgres.txt
