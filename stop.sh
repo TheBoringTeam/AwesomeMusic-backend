164e4d5ce56b6bec8e4125f242c40678a04165cf
HolkerDev <nick.penkovskiy@gmail.com>
2020-04-29 17:33:52 +0200

    Fixed stop script for Jenkins

--- stop.sh
+++ stop.sh
@@ -1,1 +1,6 @@
-docker stop backend
+if [ ! "$(docker ps -q -f name=backend)" ]; then
+    if [ "$(docker ps -aq -f status=exited -f name=backend)" ]; then
+        # cleanup
+        docker rm backend
+    fi
+fi
