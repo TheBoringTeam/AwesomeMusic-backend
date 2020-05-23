df58c416e3c464ca200746d359f34eb16cc879e4
HolkerDev <nick.penkovskiy@gmail.com>
2020-05-24 00:11:35 +0200

    Fixed sign in process validation

--- src/main/kotlin/com/music/awesomemusic/persistence/dto/request/AccountLoginForm.kt
+++ src/main/kotlin/com/music/awesomemusic/persistence/dto/request/AccountLoginForm.kt
@@ -1,13 +1,24 @@
 package com.music.awesomemusic.persistence.dto.request
 
+import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.annotation.JsonIgnore
 import com.fasterxml.jackson.annotation.JsonProperty
 import javax.validation.constraints.NotEmpty
 
-data class AccountLoginForm(
+data class AccountLoginForm @JsonCreator constructor(
         @field:JsonProperty("login")
         @field:NotEmpty(message = "Login could not be empty")
-        val login: String,
+        private val loginField: String?,
 
         @field:JsonProperty("password")
         @field:NotEmpty(message = "Password could not be empty")
-        val password: String)
+        private val passwordField: String?
+) {
+    val login: String
+        @JsonIgnore
+        get() = loginField!!
+
+    val password: String
+        @JsonIgnore
+        get() = passwordField!!
+}
