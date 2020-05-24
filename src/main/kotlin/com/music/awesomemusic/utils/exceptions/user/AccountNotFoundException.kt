d227f15adab2294337a2432ecfe085641b78bf7b
HolkerDev <nick.penkovskiy@gmail.com>
2020-05-24 17:49:29 +0200

    Error handling

--- src/main/kotlin/com/music/awesomemusic/utils/exceptions/user/AccountNotFoundException.kt
+++ src/main/kotlin/com/music/awesomemusic/utils/exceptions/user/AccountNotFoundException.kt
@@ -3,7 +3,7 @@ package com.music.awesomemusic.utils.exceptions.user
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.ResponseStatus
 
-@ResponseStatus(HttpStatus.NOT_FOUND)
+@ResponseStatus(HttpStatus.FORBIDDEN)
 class AccountNotFoundException(msg: String) : Exception(msg) {
 
 }
