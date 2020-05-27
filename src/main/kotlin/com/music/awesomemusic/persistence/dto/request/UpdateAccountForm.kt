cd4a9c44e9c1d627999fa4bce7cea14ef23fff9b
HolkerDev <nick.penkovskiy@gmail.com>
2020-05-27 22:59:11 +0200

    Added update account information form

--- src/main/kotlin/com/music/awesomemusic/persistence/dto/request/UpdateAccountForm.kt
+++ src/main/kotlin/com/music/awesomemusic/persistence/dto/request/UpdateAccountForm.kt
@@ -1,3 +1,34 @@
 package com.music.awesomemusic.persistence.dto.request
 
-data class UpdateAccountForm(val username: String)
+import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.music.awesomemusic.persistence.domain.Education
+import com.music.awesomemusic.persistence.domain.Gender
+import com.music.awesomemusic.persistence.domain.Language
+import java.util.*
+
+data class UpdateAccountForm @JsonCreator constructor(
+        @JsonProperty("username")
+        val username: String?,
+
+        @JsonProperty("name")
+        val name: String?,
+
+        @JsonProperty("biography")
+        val biography: String?,
+
+        @JsonProperty("birthday")
+        val birthday: Date?,
+
+        @JsonProperty("deathday")
+        val deathday: Date?,
+
+        @JsonProperty("gender")
+        val gender: Gender?,
+
+        @JsonProperty("education")
+        val education: Education?,
+
+        @JsonProperty("country_id")
+        val language: Language?
+)
