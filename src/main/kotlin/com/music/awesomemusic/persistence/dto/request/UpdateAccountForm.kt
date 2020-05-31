a700edc1ad3bba3bbc7ae415f2d3c73c5a834f34
HolkerDev <nick.penkovskiy@gmail.com>
2020-05-31 21:38:17 +0200

    Added dates validation for update user profile

--- src/main/kotlin/com/music/awesomemusic/persistence/dto/request/UpdateAccountForm.kt
+++ src/main/kotlin/com/music/awesomemusic/persistence/dto/request/UpdateAccountForm.kt
@@ -2,30 +2,33 @@ package com.music.awesomemusic.persistence.dto.request
 
 import com.fasterxml.jackson.annotation.JsonCreator
 import com.fasterxml.jackson.annotation.JsonProperty
-import com.music.awesomemusic.persistence.domain.Education
-import com.music.awesomemusic.persistence.domain.Gender
 import com.music.awesomemusic.utils.validators.annotations.ExistsCountryCode
 import com.music.awesomemusic.utils.validators.annotations.ExistsEducation
 import com.music.awesomemusic.utils.validators.annotations.ExistsGender
 import com.music.awesomemusic.utils.validators.annotations.ExistsLanguageCode
-import java.util.*
+import javax.validation.constraints.Pattern
+import javax.validation.constraints.Size
 
 data class UpdateAccountForm @JsonCreator constructor(
         @JsonProperty("name")
+        @field:Size(max = 255, message = "Name can be max 255 chars long")
         val name: String?,
 
         @JsonProperty("biography")
+        @field:Size(max = 1024, message = "Biography can be max 1024 chars long")
         val biography: String?,
 
         @JsonProperty("birthday")
-        val birthday: Date?,
+        @field:Pattern(regexp = "^\\d{2}/\\d{2}/\\d{4}\$", message = "Invalid birth date format")
+        val birthday: String?,
 
         @JsonProperty("deathday")
-        val deathday: Date?,
+        @field:Pattern(regexp = "^\\d{2}/\\d{2}/\\d{4}\$", message = "Invalid death date format")
+        val deathday: String?,
 
         @JsonProperty("gender")
         @ExistsGender
-        val gender: Gender?,
+        val gender: String?,
 
         @JsonProperty("education")
         @ExistsEducation
