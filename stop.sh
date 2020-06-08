e162f6a40b30faa015618aca80fd140fc56bfb8a
HolkerDev <nick.penkovskiy@gmail.com>
2020-06-08 22:24:13 +0200

    Fixed stop script

--- stop.sh
+++ stop.sh
@@ -1,6 +1,2 @@
-if [ -z `docker ps -q --no-trunc | grep $(docker-compose ps -q backend)` ]; then
-  docker kill backend
-  echo "No, it's not running."
-else
-  echo "Yes, it's running."
-fi
+docker stop backend
+docker stop awemusic-backend_database_1
