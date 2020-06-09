1cb544596b8eedec6c009502522d992f7ffb6163
HolkerDev <nick.penkovskiy@gmail.com>
2020-06-10 00:56:00 +0200

    Added song entity

---
+++ src/main/kotlin/com/music/awesomemusic/persistence/domain/Song.kt
@@ -0,0 +1,38 @@
+package com.music.awesomemusic.persistence.domain
+
+import java.time.LocalDateTime
+import javax.persistence.*
+
+@Entity
+@Table(name = "song")
+data class Song(
+        @Column(name = "song_name")
+        var name: String,
+
+        @Column(name = "song_path")
+        var path: String,
+
+        @Column(name = "length")
+        var length: Int,
+
+        @ManyToOne
+        @JoinColumn(name = "song_owner_id")
+        var owner: SongOwner,
+
+        @Column(name = "song_image")
+        var image: String
+) {
+    @Id
+    @Column(name = "song_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    val id: Long = 0
+
+    @Column(name = "created_at")
+    val createdAt: LocalDateTime = LocalDateTime.now()
+
+    @Column(name = "disabled_at")
+    var disabledAt: LocalDateTime? = null
+
+    @Column(name = "song_listen_count")
+    var counter: Long = 0
+}
---
+++ src/main/kotlin/com/music/awesomemusic/persistence/domain/SongOwner.kt
@@ -0,0 +1,25 @@
+package com.music.awesomemusic.persistence.domain
+
+import java.time.LocalDateTime
+import javax.persistence.*
+
+@Entity
+@Table(name = "song_owner")
+data class SongOwner(
+        @Column(name = "song_owner_name")
+        var name: String,
+
+        @Column(name = "song_owner_email")
+        var email: String,
+
+        @Column(name = "comment")
+        var comment: String
+) {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "song_onwer_id")
+    val id: Long = 0
+
+    @Column(name = "created_at")
+    val createdAt: LocalDateTime = LocalDateTime.now()
+}
